b4d4169a4b02b0c0b7376e0c10b53688e9cae33a
raveesha <it22271150@my.sliit.lk>
2024-05-15 09:17:33 +0530

    updated2

--- app/src/main/java/com/example/madlab4/Note.kt
+++ app/src/main/java/com/example/madlab4/Note.kt
@@ -1,3 +1,2 @@
 package com.example.madlab4
-
 data class Note(val id:Int,val title:String,val content:String)
--- app/src/main/java/com/example/madlab4/NotesDatabaseHelper.kt
+++ app/src/main/java/com/example/madlab4/NotesDatabaseHelper.kt
@@ -10,19 +10,19 @@ class NotesDatabaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE
         private const val DATABASE_NAME = "notesapp.db"
         private const val DATABASE_VERSION = 1
         private const val TABLE_NAME = "allnotes"
-        private const val COLUMN_ID = "_id"
+        private const val COLUMN_ID = "id"
         private const val COLUMN_TITLE = "title"
         private const val COLUMN_CONTENT = "content"
     }
 
-    override fun onCreate(db: SQLiteDatabase) {
-        val createTableQuery = ("CREATE TABLE $TABLE_NAME ($COLUMN_ID INTEGER PRIMARY KEY AUTOINCREMENT, "
-                + "$COLUMN_TITLE TEXT, $COLUMN_CONTENT TEXT)")
-        db.execSQL(createTableQuery)
+    override fun onCreate(db: SQLiteDatabase?) {
+        val createTableQuery = "CREATE TABLE $TABLE_NAME ($COLUMN_ID INTEGER PRIMARY KEY,$COLUMN_TITLE TEXT, $COLUMN_CONTENT TEXT)"
+        db?.execSQL(createTableQuery)
     }
 
-    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
-        db.execSQL("DROP TABLE IF EXISTS $TABLE_NAME")
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        val dropTableQuery=("DROP TABLE IF EXISTS $TABLE_NAME")
+        db?.execSQL(dropTableQuery)
         onCreate(db)
     }
     fun insertNote(note: Note){
